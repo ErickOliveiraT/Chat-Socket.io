712947277123efcf69cb3934759f37f33f029a09
Briuor <briuorlol89@gmail.com>
2020-06-03 21:43:04 -0300

    prototipo da parte de grupo feita

--- public/client.js
+++ public/client.js
@@ -3,15 +3,31 @@ var socket = io.connect('http://localhost:4000');
 var message = document.getElementById('message'),
 handle = document.getElementById('handle'),
 btn = document.getElementById('send'),
-output = document.getElementById('output');
+    output = document.getElementById('output');
 
-btn.addEventListener('click', function () {
+var currentGroup = "grupo1";
+
+btn.addEventListener('click', function (e) {
+    e.preventDefault();
     socket.emit('chat', {
         message: message.value,
-        handle: handle.value
+        handle: handle.value,
+        group: currentGroup
     });
     message.value = "";
 });
+
+const $groupButtons = $('button.group');
+$groupButtons.on('click', (e) => {
+    e.preventDefault();
+    $groupButtons.removeClass('group-active');
+    $(e.target).addClass('group-active');
+
+    //muda de grupo 
+    const newGroupName = e.target.id;
+    socket.emit('change_group', { handle: handle.value, oldGroup: currentGroup, newGroup: newGroupName});
+    currentGroup = newGroupName;
+})
 
 socket.on('chat', function(data) {
     output.innerHTML += '<p><strong>' + data.handle + ': </strong>' + data.message + '</p>';
